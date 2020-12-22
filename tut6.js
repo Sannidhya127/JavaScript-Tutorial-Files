60cdfa5e23137273c1d8a5a5653603a0798e0dcd
Sannidhya Dasgupta <72875652+Sannidhya4396@users.noreply.github.com>
2020-12-22 14:38:53 +0530

    Created the repo...........

---
+++ tut6.js
@@ -0,0 +1,24 @@
+let temp = "This is a temp variable";
+console.log(temp);
+
+// Creating the function
+
+function greet(name, GreetText) {
+    let name1 = "Local";
+    // console.log("How ar you!)
+    console.log(`Hi beta ${name}. ${GreetText}`);
+}
+let GreetText = "Hello Habla ";
+greet("Sannidhya", GreetText);
+
+
+// Creating a new function
+
+function add(a, b, c) {
+    let d = a + b + c;
+    // console.log(d)
+    return d;
+}
+
+addend = add(9, 456, 567)
+console.log(addend);
